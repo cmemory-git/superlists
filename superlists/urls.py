030d19d9c614cb441d351ce2e881af6ba4f04d16
常忆 <changyi@changyideMacBook-Pro.local>
2016-08-31 23:38:06 +0800

    Basic view now returns minimal HTML.
    Whatever doesn't kill you makes you stronger.

--- superlists/urls.py
+++ superlists/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-import lists
+
+from lists import views
+
 
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
-    url(r'^$', lists.views.home_page, name='home'),
+    url(r'^$', views.home_page, name='home'),
 ]
