25147b1f26178eb65058d8d771ee578bd7996811
常忆 <changyi@changyideMacBook-Pro.local>
2016-08-31 23:16:42 +0800

    First unit test and url mapping, dummy view.
    Whatever doesn't kill you makes you stronger.

--- functional_test.py
+++ functional_test.py
@@ -4,7 +4,7 @@ import unittest
 from selenium import webdriver
 
 
-os.environ['SELENIUM_SERVER_JAR'] = '/Users/changyi/projects/eclipse-workspace/tdd-python/support/selenium-server-standalone-3.0.0-beta2.jar'
+os.environ['SELENIUM_SERVER_JAR'] = '/Users/changyi/projects/eclipse-workspace/superlists/support/selenium-server-standalone-3.0.0-beta2.jar'
 
 class NewVisitorTest(unittest.TestCase):
     
