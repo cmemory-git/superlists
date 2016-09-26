63f706f77ee858403964f5bbbc83c3158a6e1522
常忆 <changyi@changyideMacBook-Pro.local>
2016-09-26 23:33:14 +0800

    add a new view method to create lists

--- lists/views.py
+++ lists/views.py
@@ -4,11 +4,14 @@ from lists.models import Item
 
 KEY_ENTER = b'\xEE\x80\x87'.decode()
 # Create your views here.
-def home_page(request):
-    if request.method == 'POST':
-        trimmedInput = request.POST['item_text'].rstrip(KEY_ENTER)
-        Item.objects.create(text = trimmedInput)
-        return redirect('/')
-    
+def home_page(request):    
+    return render(request, 'home.html')
+
+def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    trimmedInput = request.POST['item_text'].rstrip(KEY_ENTER)
+    Item.objects.create(text = trimmedInput)
+    return redirect('/lists/the-only-list-in-the-world/')
