63be3f2f688f51e603f5f4348a384e8406a8f7a9
常忆 <changyi@changyideMacBook-Pro.local>
2016-10-01 10:49:27 +0800

    new URL + view for adding to existing lists. FT passes.
    Whatever doesn't kill you makes you stronger.

--- lists/views.py
+++ lists/views.py
@@ -7,12 +7,18 @@ KEY_ENTER = b'\xEE\x80\x87'.decode()
 def home_page(request):    
     return render(request, 'home.html')
 
-def view_list(request):
-    items = Item.objects.all()
-    return render(request, 'list.html', {'items': items})
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list': list_})
 
 def new_list(request):
     list_ = List.objects.create()
     trimmedInput = request.POST['item_text'].rstrip(KEY_ENTER)
     Item.objects.create(text=trimmedInput, list=list_)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect('/lists/%d/' % (list_.id,))
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    trimmedInput = request.POST['item_text'].rstrip(KEY_ENTER)
+    Item.objects.create(text=trimmedInput, list=list_)
+    return redirect('/lists/%d/' % (list_.id,))
